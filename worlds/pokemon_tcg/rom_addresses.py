d72184c6f9a51cdb2fb028fe366f90b3e58de670
ProfBytes <snyde479@umn.edu>
2025-12-30 17:09:12 -0600

    testing

--- worlds/pokemon_tcg/rom_addresses.py
+++ worlds/pokemon_tcg/rom_addresses.py
@@ -5,7 +5,7 @@ rom_addresses = {
     "Starter Deck": 0x309a0,
     "Door Setting": 0x03ff6,
     "Door Strings Start": 0x64bde, #Must be 28 bytes, all in a row
-    "Door Requirements Start": 0x6835a, #first 8 bytes is type (0 = medal, 1 = card), next 8 are the ID
+    "Door Requirements Start": 0x68000, #first 8 bytes is type (0 = medal, 1 = card), next 8 are the ID
     "Reward Table": 0x6836a, #2 bytes per trainer for first and second matches.  Need more details to fill out
     "AP Item Table": 0x6841c, # 2 bytes per trainer.  Order is trainer first, empty slots, trainer second, empty, medal replacement, mail, trade
     "AP Item Type": 0x684ce, # 2 bytes per trainer.  +1 for filler, +2 for prog, +4 for useful, +8 for trap, $ff for no item
