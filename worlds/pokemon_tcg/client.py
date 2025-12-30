8fc0b64e7ac9312be28116c244b499612ec9c0e3
ProfBytes <snyde479@umn.edu>
2025-12-29 23:30:31 -0600

    Locations can be checked

--- worlds/pokemon_tcg/client.py
+++ worlds/pokemon_tcg/client.py
@@ -14,15 +14,15 @@ logger = logging.getLogger("Client")
 BANK_EXCHANGE_RATE = 50000000
 
 DATA_LOCATIONS = {
-    "ItemIndex": (0xdf2a, 0x02),
+    "ItemIndex": (0x1f2a, 0x02),
 #    "Deathlink": (0x00FD, 0x01),
-    "APItem": (0xdf29, 0x01),
-#    "EventFlag": (0x1735, 0x140),
+    "APItem": (0x1f29, 0x01),
+    "EventFlag": (0x13ef, 0x0E),
 #    "Missable": (0x161A, 0x20),
 #    "Hidden": (0x16DE, 0x0E),
 #    "Rod": (0x1716, 0x01),
 #    "DexSanityFlag": (0x1A71, 19),
-    "GameStatus": (0xdf2b, 0x01),
+    "GameStatus": (0x1f2b, 0x01),
 #    "Money": (0x141F, 3),
 #    "CurrentMap": (0x1436, 1),
 #    "ResetCheck": (0x0100, 4),
@@ -37,11 +37,12 @@ DATA_LOCATIONS = {
 #    "CrashCheck4": (0x16DD, 1),
 }
 
-location_map = {"Rod": {}, "EventFlag": {}, "Missable": {}, "Hidden": {}, "list": {}, "DexSanityFlag": {}}
+location_map = {}
 location_bytes_bits = {}
 for location in location_data:
     if location.ram_address is not None:
-        location_bytes_bits[location.address] = {'byte': location.ram_address, 'bit': location.bit_mask}
+        location_map[location.name] = False
+        location_bytes_bits[location.name] = {'byte': location.ram_address, 'bit': location.bit_mask}
 
 location_name_to_id = {location.name: location.address for location in location_data if location.type == "Item"
                        and location.address is not None}
@@ -87,6 +88,10 @@ class PokemonTCGClient(BizHawkClient):
             auth_name = auth_name[0].decode("ascii").split("\xff")[0]
         else:
             auth_name = base64.b64encode(auth_name[0]).decode()
+
+        auth_name = await read(ctx.bizhawk_ctx, [(rom_addresses["Slot Name"], 16, "ROM")])
+        print(auth_name)
+        auth_name = bytearray([b for b in auth_name[0] if b <= 127]).decode("ascii")
         ctx.auth = auth_name
 
     async def game_watcher(self, ctx):
@@ -97,29 +102,29 @@ class PokemonTCGClient(BizHawkClient):
                                             for loc_data in DATA_LOCATIONS.values()])
         data = {data_set_name: data_name for data_set_name, data_name in zip(DATA_LOCATIONS.keys(), data)}
 
-        if self.set_deathlink:
-            self.set_deathlink = False
-            await ctx.update_death_link(True)
+        # if self.set_deathlink:
+        #     self.set_deathlink = False
+        #     await ctx.update_death_link(True)
 
         if self.disconnect_pending:
             self.disconnect_pending = False
             await ctx.disconnect()
 
-        if data["GameStatus"][0] == 0 or data["ResetCheck"] == b'\xff\xff\xff\x7f':
-            # Do not handle anything before game save is loaded
-            self.game_state = False
-            return
-        elif (data["GameStatus"][0] not in (0x2A, 0xAC)
-              or data["CrashCheck1"][0] & 0xF0 or data["CrashCheck1"][1] & 0xFF
-              or data["CrashCheck2"][0]
-              or data["CrashCheck3"][0] > 10
-              or data["CrashCheck4"][0] > 3):
-            # Should mean game crashed
-            logger.warning("Pokémon TCG game may have crashed. Disconnecting from server.")
-            self.game_state = False
-            await ctx.disconnect()
-            return
-        self.game_state = True
+        # if data["GameStatus"][0] == 0 or data["ResetCheck"] == b'\xff\xff\xff\x7f':
+        #     # Do not handle anything before game save is loaded
+        #     self.game_state = False
+        #     return
+        # elif (data["GameStatus"][0] not in (0x2A, 0xAC)
+        #       or data["CrashCheck1"][0] & 0xF0 or data["CrashCheck1"][1] & 0xFF
+        #       or data["CrashCheck2"][0]
+        #       or data["CrashCheck3"][0] > 10
+        #       or data["CrashCheck4"][0] > 3):
+        #     # Should mean game crashed
+        #     logger.warning("Pokémon TCG game may have crashed. Disconnecting from server.")
+        #     self.game_state = False
+        #     await ctx.disconnect()
+        #     return
+        # self.game_state = True
 
         # SEND ITEMS TO CLIENT
 
@@ -136,16 +141,19 @@ class PokemonTCGClient(BizHawkClient):
 
         locations = set()
 
-        for flag_type, loc_map in location_map.items():
-            for flag, loc_id in loc_map.items():
-                if flag_type == "list":
-                    if (data["EventFlag"][location_bytes_bits[loc_id][0]['byte']] & 1 <<
-                            location_bytes_bits[loc_id][0]['bit']
-                            and data["Missable"][location_bytes_bits[loc_id][1]['byte']] & 1 <<
-                            location_bytes_bits[loc_id][1]['bit']):
-                        locations.add(loc_id)
-                elif data[flag_type][location_bytes_bits[loc_id]['byte']] & 1 << location_bytes_bits[loc_id]['bit']:
-                    locations.add(loc_id)
+        for location_name, found in location_map.items():
+            if not found and data["EventFlag"][(location_bytes_bits[location_name]['byte'])-0x13ef] & location_bytes_bits[location_name]['bit'] > 0:
+                locations.add(location_name_to_id[location_name])
+                location_map[location_name] = True
+            # for flag, loc_id in loc_map.items():
+            #     if flag_type == "list":
+            #         if (data["EventFlag"][location_bytes_bits[loc_id][0]['byte']] & 1 <<
+            #                 location_bytes_bits[loc_id][0]['bit']
+            #                 and data["Missable"][location_bytes_bits[loc_id][1]['byte']] & 1 <<
+            #                 location_bytes_bits[loc_id][1]['bit']):
+            #             locations.add(loc_id)
+            #     elif data[flag_type][location_bytes_bits[loc_id]['byte']] & 1 << location_bytes_bits[loc_id]['bit']:
+            #         locations.add(loc_id)
 
         if locations != self.locations_array:
             if locations:
@@ -155,55 +163,55 @@ class PokemonTCGClient(BizHawkClient):
         # AUTO HINTS
 
         hints = []
-        if data["EventFlag"][280] & 16:
-            hints.append("Cerulean Bicycle Shop")
-        if data["EventFlag"][280] & 32:
-            hints.append("Route 2 Gate - Oak's Aide")
-        if data["EventFlag"][280] & 64:
-            hints.append("Route 11 Gate 2F - Oak's Aide")
-        if data["EventFlag"][280] & 128:
-            hints.append("Route 15 Gate 2F - Oak's Aide")
-        if data["EventFlag"][281] & 1:
-            hints += ["Celadon Prize Corner - Item Prize 1", "Celadon Prize Corner - Item Prize 2",
-                      "Celadon Prize Corner - Item Prize 3"]
-        if (location_name_to_id["Fossil - Choice A"] in ctx.checked_locations and location_name_to_id[
-            "Fossil - Choice B"]
-                not in ctx.checked_locations):
-            hints.append("Fossil - Choice B")
-        elif (location_name_to_id["Fossil - Choice B"] in ctx.checked_locations and location_name_to_id[
-            "Fossil - Choice A"]
-              not in ctx.checked_locations):
-            hints.append("Fossil - Choice A")
-        hints = [
-            location_name_to_id[loc] for loc in hints if location_name_to_id[loc] not in self.auto_hints and
-                                                         location_name_to_id[loc] in ctx.missing_locations and
-                                                         location_name_to_id[loc] not in ctx.locations_checked
-        ]
-        if hints:
-            await ctx.send_msgs([{"cmd": "LocationScouts", "locations": hints, "create_as_hint": 2}])
-        self.auto_hints.update(hints)
+        # if data["EventFlag"][280] & 16:
+        #     hints.append("Cerulean Bicycle Shop")
+        # if data["EventFlag"][280] & 32:
+        #     hints.append("Route 2 Gate - Oak's Aide")
+        # if data["EventFlag"][280] & 64:
+        #     hints.append("Route 11 Gate 2F - Oak's Aide")
+        # if data["EventFlag"][280] & 128:
+        #     hints.append("Route 15 Gate 2F - Oak's Aide")
+        # if data["EventFlag"][281] & 1:
+        #     hints += ["Celadon Prize Corner - Item Prize 1", "Celadon Prize Corner - Item Prize 2",
+        #               "Celadon Prize Corner - Item Prize 3"]
+        # if (location_name_to_id["Fossil - Choice A"] in ctx.checked_locations and location_name_to_id[
+        #     "Fossil - Choice B"]
+        #         not in ctx.checked_locations):
+        #     hints.append("Fossil - Choice B")
+        # elif (location_name_to_id["Fossil - Choice B"] in ctx.checked_locations and location_name_to_id[
+        #     "Fossil - Choice A"]
+        #       not in ctx.checked_locations):
+        #     hints.append("Fossil - Choice A")
+        # hints = [
+        #     location_name_to_id[loc] for loc in hints if location_name_to_id[loc] not in self.auto_hints and
+        #                                                  location_name_to_id[loc] in ctx.missing_locations and
+        #                                                  location_name_to_id[loc] not in ctx.locations_checked
+        # ]
+        # if hints:
+        #     await ctx.send_msgs([{"cmd": "LocationScouts", "locations": hints, "create_as_hint": 2}])
+        # self.auto_hints.update(hints)
 
         # DEATHLINK
 
-        if "DeathLink" in ctx.tags:
-            if data["Deathlink"][0] == 3:
-                await ctx.send_death(ctx.player_names[ctx.slot] + " is out of usable Pokémon! "
-                                     + ctx.player_names[ctx.slot] + " blacked out!")
-                await write(ctx.bizhawk_ctx, [(DATA_LOCATIONS["Deathlink"][0], [0], "WRAM")])
-                self.last_death_link = ctx.last_death_link
-            elif ctx.last_death_link > self.last_death_link:
-                self.last_death_link = ctx.last_death_link
-                await write(ctx.bizhawk_ctx, [(DATA_LOCATIONS["Deathlink"][0], [1], "WRAM")])
-
-        if data["CurrentMap"][0] != self.current_map:
-            await ctx.send_msgs([{"cmd": "Bounce", "slots": [ctx.slot], "data": {"currentMap": data["CurrentMap"][0]}}])
-            self.current_map = data["CurrentMap"][0]
+        # if "DeathLink" in ctx.tags:
+        #     if data["Deathlink"][0] == 3:
+        #         await ctx.send_death(ctx.player_names[ctx.slot] + " is out of usable Pokémon! "
+        #                              + ctx.player_names[ctx.slot] + " blacked out!")
+        #         await write(ctx.bizhawk_ctx, [(DATA_LOCATIONS["Deathlink"][0], [0], "WRAM")])
+        #         self.last_death_link = ctx.last_death_link
+        #     elif ctx.last_death_link > self.last_death_link:
+        #         self.last_death_link = ctx.last_death_link
+        #         await write(ctx.bizhawk_ctx, [(DATA_LOCATIONS["Deathlink"][0], [1], "WRAM")])
+        #
+        # if data["CurrentMap"][0] != self.current_map:
+        #     await ctx.send_msgs([{"cmd": "Bounce", "slots": [ctx.slot], "data": {"currentMap": data["CurrentMap"][0]}}])
+        #     self.current_map = data["CurrentMap"][0]
 
         # VICTORY
 
-        if data["EventFlag"][280] & 1 and not ctx.finished_game:
-            await ctx.send_msgs([{"cmd": "StatusUpdate", "status": ClientStatus.CLIENT_GOAL}])
-            ctx.finished_game = True
+        # if data["EventFlag"][280] & 1 and not ctx.finished_game:
+        #     await ctx.send_msgs([{"cmd": "StatusUpdate", "status": ClientStatus.CLIENT_GOAL}])
+        #     ctx.finished_game = True
 
     def on_package(self, ctx, cmd, args):
         if cmd == 'Connected':
